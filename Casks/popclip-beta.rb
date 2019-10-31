b1df32484470a394bbac85410632526ea36f3b68
aukaheng <aukhhome@hotmail.com>
2019-11-01 00:31:18 +0800

    Fix the hash value

--- Casks/popclip-beta.rb
+++ Casks/popclip-beta.rb
@@ -1,6 +1,6 @@
 cask 'popclip-beta' do
   version '3015'
-  sha256 'FC21BB75AA97CBFCAA645C1CE4862BE41C9ABCF82B97AA953F0EB09226A56BA7'
+  sha256 'fc21bb75aa97cbfcaa645c1ce4862be41c9abcf82b97aa953f0eb09226a56ba7'
 
   url "https://pilotmoon.com/downloads/beta/PopClip-build-#{version}.zip"
   name 'PopClip'
