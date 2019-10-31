a39d85dabd12a91555f99d722f8b2072f27c4af6
aukaheng <aukhhome@hotmail.com>
2019-11-01 01:00:57 +0800

    Update popclip-beta.rb

--- Casks/popclip-beta.rb
+++ Casks/popclip-beta.rb
@@ -1,6 +1,6 @@
 cask 'popclip-beta' do
   version '3022'
-  sha256 'f58e147a6fef4016b4fb74827173c8d85056cfe8'
+  sha256 'd58dec3e8df38ff2b8fa9806037cd42207c0301ec4eaf7e32b41006716a86fe6'
 
   url "https://pilotmoon.com/downloads/beta/PopClip-build-#{version}.zip"
   name 'PopClip'
