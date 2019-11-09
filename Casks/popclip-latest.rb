c5fd9a3b8133ad6ab8d27aed12cab3b9c197e20d
aukaheng <aukhhome@hotmail.com>
2019-11-09 13:27:55 +0800

    Fix the hash value case sensitive issue

--- Casks/popclip-latest.rb
+++ Casks/popclip-latest.rb
@@ -1,6 +1,6 @@
 cask 'popclip-latest' do
   version '2019.10'
-  sha256 '052CB6D24D1060FCC133837603C8D48BC937F3C0D9AD34EEA6F695AEDFFB50E4'
+  sha256 '052cb6d24d1060fcc133837603c8d48bc937f3c0d9ad34eea6f695aedffb50e4'
 
   url "https://pilotmoon.com/downloads/PopClip-#{version}.zip"
   name 'PopClip'
