73bccaa2f1789008285eede24e0b76e6da60dae4
aukaheng <aukhhome@hotmail.com>
2019-12-20 12:37:36 +0800

    Update Mos beta to 3.0.0

--- Casks/mos-beta.rb
+++ Casks/mos-beta.rb
@@ -1,8 +1,8 @@
 cask 'mos-beta' do
-  version '2.4.0'
-  sha256 'b52040a051fd073576bc056d2f8af86e309cb37ad94ca3f0230a9f4a2538c24e'
+  version '3.0.0'
+  sha256 '274f984663696c59a3d69b959a4ae9a11c25e171573357e264784e04f9d2d27c'
 
-  url "https://github.com/Caldis/Mos/releases/download/#{version}-beta/Mos.Versions.#{version}-beta.dmg"
+  url "https://raw.githubusercontent.com/Caldis/Mos/master/Release/Mos.Versions.3.0.0.dmg"
   name 'Mos'
   homepage 'https://mos.caldis.me/'
 
