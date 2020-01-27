fe64ebf6ae891ee61bd2404b24832be0d39a5ee1
aukaheng <aukhhome@hotmail.com>
2020-01-27 23:31:05 +0800

    Add BetterZip beta 4.2.6 beta 1

---
+++ Casks/betterzip-beta.rb
@@ -0,0 +1,23 @@
+cask 'betterzip-beta' do
+  version '4.2.6b1'
+  sha256 'e257e94fcb378fd9b24b2f2138a85e930fb44c26'
+
+  url "https://macitbetter.com/dl/BetterZip-#{version}.zip"
+  name 'BetterZip'
+  homepage 'https://macitbetter.com/'
+
+  app 'BetterZip.app'
+
+  zap trash: [
+    '~/Library/Application Scripts/com.macitbetter.betterzip.findersyncextension',
+    '~/Library/Application Support/com.apple.sharedfilelist/com.apple.LSSharedFileList.ApplicationRecentDocuments/com.macitbetter.betterzip.sfl*',
+    '~/Library/Application Support/com.macitbetter.betterzip',
+    '~/Library/Caches/com.apple.helpd/Generated/com.macitbetter.betterzip.help*',
+    '~/Library/Caches/com.macitbetter.betterzip',
+    '~/Library/Containers/com.macitbetter.betterzip.findersyncextension',
+    '~/Library/Group Containers/79RR9LPM2N.group.com.macitbetter.betterzip',
+    '~/Library/Group Containers/79RR9LPM2N.group.com.macitbetter.betterzip-setapp',
+    '~/Library/Preferences/com.macitbetter.betterzip.plist',
+    '~/Library/Saved Application State/com.macitbetter.betterzip.savedState',
+  ]
+end
