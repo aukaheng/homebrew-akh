93e65c463974ca8231e1b6e144f542bf54a5c629
aukaheng <aukhhome@hotmail.com>
2020-01-28 00:26:43 +0800

    Get the exact version

--- Casks/goodsync-beta.rb
+++ Casks/goodsync-beta.rb
@@ -1,5 +1,5 @@
 cask 'goodsync-beta' do
-  version '11'
+  version '11.0.1.1'
   sha256 '5cd2cddd067be98220290db5f67b1be55b6cb357e37ede50d82df946d10ad32a'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
