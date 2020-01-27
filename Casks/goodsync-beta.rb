ce099772bf89d228ad266d8863e09827a5310ce5
aukaheng <aukhhome@hotmail.com>
2020-01-28 00:21:57 +0800

    Add GoodSync beta 11

---
+++ Casks/goodsync-beta.rb
@@ -0,0 +1,10 @@
+cask 'goodsync-beta' do
+  version '11'
+  sha256 '5cd2cddd067be98220290db5f67b1be55b6cb357e37ede50d82df946d10ad32a'
+
+  url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
+  name 'GoodSync'
+  homepage 'https://www.goodsync.com/'
+  
+  app 'GoodSync.app'
+end
