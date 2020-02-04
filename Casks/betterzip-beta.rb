3210b7b5797636d69e143d7a0348f14ffd229dd7
aukaheng <aukhhome@hotmail.com>
2020-02-05 01:23:34 +0800

    Update betterzip-beta.rb

--- Casks/betterzip-beta.rb
+++ Casks/betterzip-beta.rb
@@ -1,5 +1,5 @@
 cask 'betterzip-beta' do
-  version '4.2.6, 1583'
+  version '4.2.6-1583'
   sha256 '87cd468569633628c983e63c558aa3af4ead105f6fb00edc1ed178d339122b0b'
 
   url "https://macitbetter.com/dl/BetterZip-#{version.major_minor}.zip"
