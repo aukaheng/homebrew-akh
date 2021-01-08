8f9c3e5832285aaf7d01d482e19e54b28ccbe296
aukaheng <aukhhome@hotmail.com>
2021-01-08 22:02:19 +0800

    Fix wrong hash value

--- Casks/goodsync10.rb
+++ Casks/goodsync10.rb
@@ -1,6 +1,6 @@
 cask 'goodsync10' do
   version '10.15.4'
-  sha256 '2ffc89b317a7f7689ad8a0708f853ac2c8ff241ca41cb7c5ea48cd385dd04a35'
+  sha256 '569a6432e867e2c3f75024de95b12d718ace827212e88df98f5dc54974195d44'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name 'GoodSync'
