a1341947aa85625e08b76adef361c62e0633f4cd
aukaheng <aukhhome@hotmail.com>
2021-06-14 15:14:09 +0800

    Update GoodSync 10 to 10.17.1

--- Casks/goodsync10.rb
+++ Casks/goodsync10.rb
@@ -1,6 +1,6 @@
 cask 'goodsync10' do
-  version '10.16.1'
-  sha256 'b835cb7a4fd7e56b9c746461651135f258b829448942e70a55a47c796b9f6cf1'
+  version '10.17.1'
+  sha256 'b1dcef78693dadc7ddbd544d50229397217a624dbe744195b0cab92f29dd5d31'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name 'GoodSync'
