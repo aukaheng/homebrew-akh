35ce30ec665a84d2cf6eb0f0c8ae0cb6ef0cb2e8
aukaheng <aukhhome@hotmail.com>
2021-12-25 15:24:00 +0800

    Update GoodSync 10 to 10.9.8

--- Casks/goodsync10.rb
+++ Casks/goodsync10.rb
@@ -1,6 +1,6 @@
 cask 'goodsync10' do
-  version '10.19.1'
-  sha256 '1208d29ddd23051c189ff9ede3ca8d098934d120724981b92763845f1a936cf8'
+  version '10.19.8'
+  sha256 'effe432e4c2e3eca9edc2fadf61380402b902ac3b937dd945ded0b94accb5d2f'
 
   url "https://www.goodsync.com/download/goodsync-v#{version.major}-mac.dmg"
   name 'GoodSync'
