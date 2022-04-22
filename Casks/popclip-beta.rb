3536aa0019bb0daebf8b9b766cebddc22edea84d
aukaheng <aukhhome@hotmail.com>
2022-04-23 01:47:09 +0800

    Update PopClip beta to 3844

--- Casks/popclip-beta.rb
+++ Casks/popclip-beta.rb
@@ -1,6 +1,6 @@
 cask 'popclip-beta' do
-  version '3835'
-  sha256 '7cb0f7e012103670e95f21b2b61dae43e4fc44e92da4fa783292e520200f974d'
+  version '3844'
+  sha256 'e6d0a3613f50eb3a2fa1a44a36cd88db4f27f4c8dc7d24e7feef7cf8a1d0788f'
 
   url "https://pilotmoon.com/downloads/beta/PopClip-build-#{version}.zip"
   name 'PopClip'
