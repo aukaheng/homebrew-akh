bed7641560d6a9cb779b526b49a972e1baae552f
aukaheng <aukhhome@hotmail.com>
2022-09-24 00:33:17 +0800

    Update PopClip beta to 3939

--- Casks/popclip-beta.rb
+++ Casks/popclip-beta.rb
@@ -1,10 +1,10 @@
 cask 'popclip-beta' do
-  version '3844'
-  sha256 'e6d0a3613f50eb3a2fa1a44a36cd88db4f27f4c8dc7d24e7feef7cf8a1d0788f'
+  version '3939'
+  sha256 'd989be352bbb8685f2826781dcab084f8c26a1c2b64b5012457895746298917e'
 
   url "https://pilotmoon.com/downloads/beta/PopClip-build-#{version}.zip"
   name 'PopClip'
-  homepage 'https://pilotmoon.com/popclip/'
+  homepage 'https://pilotmoon.com/popclip/download'
 
   app 'PopClip.app'
 end
