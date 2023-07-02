b38332473a61a75455ee64fb49edfe8b33f18c64
aukaheng <aukhhome@hotmail.com>
2023-07-02 23:13:15 +0800

    PopClip beta: Update to version 4144

--- Casks/popclip-beta.rb
+++ Casks/popclip-beta.rb
@@ -1,6 +1,6 @@
 cask 'popclip-beta' do
-  version '3946'
-  sha256 'ac9037c5c625bd555d46ecdd99a73e5837450df34be314f599d08d94f541e056'
+  version '4144'
+  sha256 '27fe56498af3952758792ffe1a873fcd89727178303618eadfbc0671a455e7cf'
 
   url "https://pilotmoon.com/downloads/beta/PopClip-build-#{version}.zip"
   name 'PopClip'
