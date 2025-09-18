27671ae8c3864e58f5ecdf9fd80942909cb52653
aukaheng <aukhhome@hotmail.com>
2025-09-18 21:07:35 +0800

    Update bartender@5.rb

--- Casks/bartender@5.rb
+++ Casks/bartender@5.rb
@@ -1,4 +1,4 @@
-cask "bartender" do
+cask "bartender@5" do
   version "5.5.0"
   sha256 "DE6BA0D663DAAD75622DBEF2004F321CF2D17C5D29F62ECA564F859F5BF33A6F"
   url "https://www.macbartender.com/B2/updates/5-5-0/Bartender%205.zip"
