d81f8b4d7bb9a913908e47a95be30dd8cf92a5b2
aukaheng <aukhhome@hotmail.com>
2025-09-28 16:35:42 +0800

    Update popclip-beta.rb

--- Casks/popclip-beta.rb
+++ Casks/popclip-beta.rb
@@ -1,10 +1,17 @@
-cask 'popclip-beta' do
-  version '4675'
-  sha256 '0c6f7da27ba099e2975fd9dacdc5049d22e280c40e1e8d6427776a683ced53bb'
+cask "popclip-beta" do
+  version "5150"
+  sha256 "53440b1a5bdd014366b58550b24dbebcc2848e5695860a0d52396ce24d5a63fe"
+  url "https://pilotmoon.com/downloads/beta/PopClip-build-5150.zip"
+  name "PopClip"
+  homepage "https://www.popclip.app/"
 
-  url "https://pilotmoon.com/downloads/beta/PopClip-build-#{version}.zip"
-  name 'PopClip'
-  homepage 'https://pilotmoon.com/popclip/download'
+  app "PopClip.app"
 
-  app 'PopClip.app'
+  zap trash: [
+    "~/Library/Application Support/com.pilotmoon.popclip",
+    "~/Library/Application Support/PopClip",
+    "~/Library/Caches/com.pilotmoon.popclip",
+    "~/Library/Preferences/com.pilotmoon.popclip.plist",
+    "~/Library/SyncedPreferences/com.apple.kvs/ChangeTokens/NoEncryption/PopClip",
+  ]
 end
