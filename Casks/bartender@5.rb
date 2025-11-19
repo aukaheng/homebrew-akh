be1e54b12f2c853a7d82bd78f55065266942777d
aukaheng <aukhhome@hotmail.com>
2025-11-19 16:55:20 +0800

    Bartender 5 beta: Update to version 5.5.8

--- Casks/bartender@5.rb
+++ Casks/bartender@5.rb
@@ -1,7 +1,7 @@
 cask "bartender@5" do
-  version "5.5.6"
-  sha256 "1f6e2899773f66539020239a86fd7219c15a10038b1a5ea1f1b05dd2374d425c"
-  url "https://www.macbartender.com/B2/updates/5-5-6/Bartender%205.dmg"
+  version "5.5.8"
+  sha256 "A424567BC8EDE46CE1C539F84CF85CB4D8E19EDDB9EE26333E59B7006891C5D3"
+  url "https://www.macbartender.com/B2/updates/5-5-8/Bartender%205.dmg"
   name "Bartender"
 
   app "Bartender 5.app"
